3f8577811e066199e6beeb02cf41184908952eb6
Joseph Essel Moses <jmosesessel21@gmail.com>
2023-05-12 00:30:52 +0000

    complete hours hand and update sec mins hours calculations

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-function updateClock(){
-    const currentDate = new Date();
-    const secs = currentDate.getSeconds()
-    const mins = currentDate.getMinutes()
-    const hours = currentDate.getHours()
+function updateClock() {
+	const currentDate = new Date();
+	const secs = currentDate.getSeconds();
+	const mins = currentDate.getMinutes() + secs / 60;
+	const hours = (currentDate.getHours() % 12) + mins / 60;
 
-    console.log('current time:', `${hours}:${mins}:${secs}`)
-    const secHandRotate = document.getElementById('seconds').setAttribute('transform', `rotate(${secs * (360/60)}, 244, 251)`)
-    const minHandRotate = document.getElementById('minutes').setAttribute('transform', `rotate(${mins * (360/60)}, 244, 251)`)
+	console.log("current time:", `${hours}:${mins}:${secs}`);
+	const secHandRotate = document
+		.getElementById("seconds")
+		.setAttribute("transform", `rotate(${secs * (360 / 60)}, 244, 251)`);
+	const minHandRotate = document
+		.getElementById("minutes")
+		.setAttribute("transform", `rotate(${mins * (360 / 60)}, 244, 251)`);
+	const hourHandRotate = document
+		.getElementById("hours")
+		.setAttribute("transform", `rotate(${hours * (360 / 12)}, 244, 251)`);
 }
 
-setInterval(()=>{
-    updateClock()
-},1000)
+setInterval(() => {
+	updateClock();
+}, 1000);
 
-// runUpdate()
